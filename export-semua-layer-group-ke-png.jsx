7020a54237544d99d3f668b49adac7b7941ef06d
zdienos <zdienos@yahoo.com>
2019-04-28 14:18:43 +0800

    update pada sisi User Interface
    update, sudah pake window, text, progressbar dan alert done

--- export-semua-layer-group-ke-png.jsx
+++ export-semua-layer-group-ke-png.jsx
@@ -1,55 +1,35 @@
-
-
-
-
-
 var destFolder = Folder.selectDialog( "Silahkan Pilih foldernya..");
     if (destFolder == null)
     {
-      throw "Batal...";
+     throw "Batal...";
     }
+
+var win = new Window("window{text:'Lagi prosees, tunggu...',bounds:[100,100,400,200],bar:Progressbar{bounds:[20,50,280,61] , value:0,maxvalue:100}};");
+win.namaFile = win.add ('statictext {text: "Nama File: ", characters: 10, justify: "center"}');
+win.testTeks  = win.add ('statictext {bounds:[10,10,300,50], text:"Loading...", justify:"center"}');    
+win.show();
 
 var doc = app.activeDocument;
 for(var i = 0 ; i < doc.layers.length-1; i++){
     var namaFilenya = doc.layers[i].name;
+    win.bar.value = (i+1)/(doc.layers.length-1)*100;
+    win.testTeks.text =  namaFilenya+".png";
     SavePNG(namaFilenya+".png");
     doc.layers[i].visible = false;    
-}    
+}
+win.close();
+alert('Selesai...');
 
 //kembalikan hideLayernya
-
 for(var i = 0 ; i < doc.layers.length-1; i++){
-    var namaFilenya = doc.layers[i].name;    
-    doc.layers[i].visible = true;   
-}    
-
-//~ for (i = 5; i > 0; i--) {
-//~    var colors = app.activeDocument.layerSets.getByName("mockup "+i);  
-//~    SavePNG("mockup "+i+".png");
-//~    colors.visible = false;
-//~ }
-
-
-
+    var namaFilenya = doc.layers[i].name;
+    doc.layers[i].visible = true;
+}
 
 function SavePNG(destFileName){
   var sfw = new ExportOptionsSaveForWeb();
       sfw.format = SaveDocumentType.PNG;
       sfw.PNG8 = false; // use PNG-24
-      sfw.transparency = true; 
+      sfw.transparency = true;
       app.activeDocument.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 }
-
-
-function tampilkanKembali(){
-var doc = app.activeDocument;
-for(var i = 0 ; i < doc.layers.length;i++){
-    doc.layers[i].visible = true
-    var xx = doc.layers[i].name;
-    alert(xx);
-}    
-    }
-
-
-
-
